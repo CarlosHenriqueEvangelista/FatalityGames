e0776f0dad1b842f50ee7a0126af9e25854ca969
CarlosHenriqueEvangelista <83401273+CarlosHenriqueEvangelista@users.noreply.github.com>
2021-09-06 17:30:52 -0300

    Try to fix GitHub page #2

--- Menu.js
+++ Menu.js
@@ -1,6 +1,6 @@
-const menu = document.querySelector('#header .menu')
-const openMenu = document.querySelector('#header .menu .icon-menu')
-const closeMenu = document.querySelector('#header .menu .icon-close')
+const menu = document.querySelector('.menu')
+const openMenu = document.querySelector('.menu .icon-menu')
+const closeMenu = document.querySelector('.menu .icon-close')
 
 openMenu.addEventListener('click', function () {
   menu.classList.add('show')
