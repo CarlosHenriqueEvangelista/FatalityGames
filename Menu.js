0d3f42d2a7be3e0233759ae726867d6b641d3d2d
CarlosHenriqueEvangelista <83401273+CarlosHenriqueEvangelista@users.noreply.github.com>
2021-09-11 15:36:58 -0300

    Post projects adjustments

--- Menu.js
+++ Menu.js
@@ -3,17 +3,17 @@ const openMenu = document.querySelector(' .menu .icon-menu')
 const closeMenu = document.querySelector(' .menu .icon-close')
 
 openMenu.addEventListener('click', function () {
-  menu.classList.add('show')
+  menu.classList.add('showMenu')
 })
 
 closeMenu.addEventListener('click', function () {
-  menu.classList.remove('show')
+  menu.classList.remove('showMenu')
 })
 
 const links = document.querySelectorAll('.menu ul li a')
 
 for (link of links) {
   link.addEventListener('click', function () {
-    menu.classList.remove('show')
+    menu.classList.remove('showMenu')
   })
 }
