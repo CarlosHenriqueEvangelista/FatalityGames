48bc51aeceb0947dc338c4dec564e1ac3148a721
CarlosHenriqueEvangelista <83401273+CarlosHenriqueEvangelista@users.noreply.github.com>
2021-09-14 09:58:51 -0300

    Post project adjustments

--- ScrollReval.js
+++ ScrollReval.js
@@ -13,7 +13,7 @@ scrollReveal.reveal(
   `#Home .image, #Home .text,
 #AboutUs .image, #AboutUs .text,
 #OurServices .text, #OurServices .cards .card,
-#WhatDoTheySayAboutUs .text, #WhatDoTheySayAboutUs .testimonials,
+#WhatDoTheySayAboutUs .text, #WhatDoTheySayAboutUs .swiper,
 #HowToFindUs .text, #HowToFindUs .maps, #HowToFindUs .contact,
 #Footer .text, #Footer .socialNetworks  #Footer .backToTopButton`,
   { interval: 100 }
